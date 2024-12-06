a0abde2b6ff1d71c25ad8f3f5af2046ba2dd4e4f
RBen19 <bonmerciel@gmail.com>
2024-12-06 15:41:38 +0100

    import dans le fichier app.compenents.ts des lignes 2 à 10 Ajout de bootsrtap creation d'un component student-add-edit ajout des imports au niveau du fichier ts du composant

--- crudApp/src/app/app.component.ts
+++ crudApp/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {StudentAddEditComponent } from './student-add-edit/student-add-edit.component';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+            MatToolbarModule,
+            MatIconModule,
+            MatButtonModule,
+            MatDialogModule,
+            MatFormFieldModule,
+            MatInputModule
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'crudApp';
+  constructor(private _dialog: MatDialog){}
+  openAddEditStudentsFrom() {
+    this._dialog.open(StudentAddEditComponent);
+  }
 }
---
+++ crudApp/src/app/student-add-edit/student-add-edit.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+@Component({
+  selector: 'app-student-add-edit',
+  imports: [MatDialogModule,
+            MatFormFieldModule,
+            MatButtonModule,
+            MatInputModule
+  ],
+  templateUrl: './student-add-edit.component.html',
+  styleUrl: './student-add-edit.component.scss'
+})
+export class StudentAddEditComponent {
+
+}
